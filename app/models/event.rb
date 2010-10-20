bb2fa5747e1aea85bc5eb217f0641af9a2aba12c
Carolyn Marshall <carolynmarshall@gmail.com>
2010-10-19 22:25:21 -0400

    basic eventbrite search

--- app/models/event.rb
+++ app/models/event.rb
@@ -19,7 +19,7 @@ class Event < ActiveRecord::Base
   end
 
   def self.event_find(query)
-    sources = [:native, :meetup]
+    sources = [:native, :eventbrite, :meetup]
 
     @results = []
     if !query.to_s.strip.empty?
@@ -59,6 +59,34 @@ class Event < ActiveRecord::Base
     end
 
     results
+  end
+
+  def self.eventbrite(q)
+    query_uri = "https://www.eventbrite.com/json/event_search?keywords=#{q}&app_key=ZDRiMTBjYWVkYjA4"
+    url = URI.parse(query_uri);
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true;
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(url.request_uri)
+    response = http.request(request)
+
+    events = JSON.parse(response.body)
+
+    results = []
+
+    events["events"].each do |event|
+
+      next if event["event"].nil?
+
+     results << Event.new(:name=> event['event']['title'],
+                :description => event['event']['description'],
+                :location => (event['event']['timezone'] rescue ""),
+                :starts_at => event['event']['start_date'],
+                :ends_at => event['event']['end_date'])
+    end
+
+    results
+
   end
 
 end
