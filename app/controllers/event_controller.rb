eafa7a9270d819b6b93994486d3d49ebd3c7e6a1
Carolyn Marshall <carolynmarshall@gmail.com>
2010-10-31 15:48:45 -0400

    add event - basic functionality so styling can start

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,6 +1,6 @@
 class EventController < ApplicationController
 
-  before_filter :login_required, :only=>[:attend, :cancel_attendance, :update_sharables, :contact_user]
+  before_filter :login_required, :only=>[:attend, :cancel_attendance, :update_sharables, :contact_user, :new, :create]
   
 def event_find
   @events = Event.event_find(params[:q], params[:loc])
@@ -17,7 +17,7 @@ end
 def attend
   event = Event.find(params[:id])
   event_group = event.event_group
-  membership = Membership.new(:user => current_user, :group => group_event, :approved => true)
+  membership = Membership.new(:user => current_user, :group => event_group, :approved => true)
   membership.save
   redirect_to event_path(event.id)
 end
@@ -55,6 +55,38 @@ def cancel_attendance
   end
   flash[:message] = 'Attendance has been removed'
   redirect_to event_path(event.id)
+end
+
+def new
+    location = params[:address] + ", " + params[:city] + ", " + params[:region]
+    unless params[:address].blank? && params[:city].blank? && params[:region].blank?
+      latlong_uri = URI.parse("http://maps.googleapis.com/maps/api/geocode/json?address=#{URI.escape(location)}&sensor=false")
+      latlong = Net::HTTP.get_response(latlong_uri)
+
+      location = JSON.parse(latlong.body)
+
+      lng = location['results'].first['geometry']['location']['lng'] rescue ""
+      lat = location['results'].first['geometry']['location']['lat'] rescue ""
+    end
+
+  event = Event.create(
+    :name=> params[:name],
+    :description => params[:description],
+    :location => params[:address] + ", " + params[:city] + ", " + params[:region],
+    :latitude => lat,
+    :longitude => lng,
+    :starts_at => params['start_time'],
+    :ends_at => params['end_time'],
+    :remote_source => "native",
+    :remote_id => nil,
+    :user_id => current_user
+  )
+
+  redirect_to event_path(:id => event.id)
+  
+end
+
+def create
 end
 
 end
