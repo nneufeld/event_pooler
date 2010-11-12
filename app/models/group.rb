6f300b3236d78b6de03e102eca909195d05d19b1
Nick Neufeld <neufelni@gmail.com>
2010-11-12 16:35:56 -0500

    Listing of groups with basic info on event page, groups closest to user appear first

--- app/models/group.rb
+++ app/models/group.rb
@@ -9,10 +9,20 @@ class Group < ActiveRecord::Base
   belongs_to :administrator, :class_name => 'User'
   has_and_belongs_to_many :sharables
 
+  acts_as_mappable :default_units => :kms,
+                   :lat_column_name => :latitude,
+                   :lng_column_name => :longitude
+
   validates_presence_of :name
 
   scope :user_groups, lambda {
     event_type = GroupType.where({:slug => 'event'}).first
     where (['group_type_id != ?', event_type.id])
+  }
+
+  scope :closest_to_user, lambda {|user|
+    unless user.nil? || user.latitude.nil? || user.longitude.nil?
+      geo_scope(:origin => [user.latitude, user.longitude]).order('distance')
+    end
   }
 end
