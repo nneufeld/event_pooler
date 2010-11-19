3e0bf4f5381467627d872477cfa4f63c978d9018
Nick Neufeld <neufelni@gmail.com>
2010-11-19 18:16:33 -0500

    Some user restriction updates

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,4 +1,7 @@
 class CommentController < ApplicationController
+  before_filter :login_required
+  before_filter :belongs_to_group, :only => [:create]
+  
   def reply
     parent_comment = Comment.find(params[:comment_id])
     @comment = Comment.new
@@ -11,5 +14,13 @@ class CommentController < ApplicationController
     @comment.group_id = params[:group_id]
     @comment.user_id = current_user.id
     @comment.save
+  end
+
+  def belongs_to_group
+    @group = Group.find(params[:group_id])
+    unless @group.users.include?(current_user)
+      flash[:message] = 'You are not a member of this group'
+      redirect_to event_path(@group.event.id) and return
+    end
   end
 end
