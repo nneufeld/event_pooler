5c50271dc8d8cb2e013c66e5a44f5a6880953514
= <carolynmarshall@gmail.com>
2010-11-22 12:54:29 -0600

    profile and ranking

---
+++ app/controllers/user_review_controller.rb
@@ -0,0 +1,11 @@
+class UserReviewController < ApplicationController
+
+  def new
+    @review = UserReview.new(params[:user_review])
+    @review.user_id = params[:id]
+    @review.reviewer_id = current_user.id
+    @review.save
+    redirect_to profile_url(@review.user_id)
+  end
+
+end
