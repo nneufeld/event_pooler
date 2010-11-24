6b1765e128551a767928df596428f16584ed57dd
Nick Neufeld <neufelni@gmail.com>
2010-11-24 17:31:10 -0500

    Some updates to search page

--- db/migrate/20101120204832_add_invite_only_group_type.rb
+++ db/migrate/20101120204832_add_invite_only_group_type.rb
@@ -7,19 +7,6 @@ class AddInviteOnlyGroupType < ActiveRecord::Migration
     gt = GroupType.new
     gt.name = 'Invite-Only'
     gt.slug = 'invite-only'
-    gt.description = 'Group can only be seen and joined by those invited by the creator.'
-    gt.save
-
-    gt = GroupType.find_by_slug('event')
-    gt.description = "Default group for an event's main page."
-    gt.save
-
-    gt = GroupType.find_by_slug('public')
-    gt.description = "Group is fully visible and can be joined by anyone."
-    gt.save
-
-    gt = GroupType.find_by_slug('private')
-    gt.description = "Group can only be viewed and joined by users approved by the creator."
     gt.save
   end
 
