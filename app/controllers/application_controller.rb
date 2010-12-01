628cb83865dae59cf6f6dd200c7551cb2ebb42a7
amcgowanca <me@amcgowan.ca>
2010-12-01 00:19:30 -0500

    Many style updates for the home page, still needs some work for non-logged in users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,6 +43,8 @@ class ApplicationController < ActionController::Base
   def call_rake(task, options = {})
     options[:rails_env] ||= Rails.env
     args = options.map { |n, v| "#{n.to_s.upcase}='#{v}'" }
+	
+	# puts "rake #{task} #{args.join(' ')} --trace 2>&1 >> #{Rails.root}/log/rake.log &"
     system "rake #{task} #{args.join(' ')} --trace 2>&1 >> #{Rails.root}/log/rake.log &"
   end
   
