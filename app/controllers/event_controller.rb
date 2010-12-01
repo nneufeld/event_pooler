34f921a66ca08befc8071799a242035613f94441
Nick Neufeld <neufelni@gmail.com>
2010-12-01 08:26:13 -0500

    Filter fix

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -131,7 +131,8 @@ class EventController < ApplicationController
       @groups = @groups.sharing(sharables_filter)
     end
     if !location_filter.empty?
-      @groups = @groups.where(['city = ? AND region = ?', location_filter[0], location_filter[1]])
+      puts location_filter[0]
+      @groups = @groups.where(['city = ?', location_filter[0]])
     end
 
   end
